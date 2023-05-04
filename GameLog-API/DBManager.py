cff725924ab49283ef2e049b5d7be5f309624b80
CharTex <HarryAGearing@outlook.com>
2023-05-04 14:49:16 +0100

    Add Create Table Code
    Adds the code responsible for creating the initial tables in the created database.

--- GameLog-API/DBManager.py
+++ GameLog-API/DBManager.py
@@ -2,6 +2,7 @@
 
 import sqlite3
 import os
+import time
 
 class DBManager():
 
@@ -15,14 +16,14 @@ class DBManager():
         self.directory_check()
         self.connected = self.attempt_connection()
 
+        if self.connected:
+            self.initialize_tables()
+
     def directory_check(self):
         # Checks to see if directories need to be created.
-        print(self.database_path)
-
         try:
             if not os.path.exists(self.database_path):
                 os.makedirs(os.path.dirname(self.database_path), exist_ok=True)
-                print("Done")
         except PermissionError:
             print("Permission Error occured in Database Read/Write.")
             print("Please run the program from a directory with full user access.")
@@ -39,6 +40,7 @@ class DBManager():
         if self.database_path is not None:
             try:
                 self.connection = sqlite3.connect(self.database_path)
+                time.sleep(0.5)
                 return True
             except Exception as e:
                 print("An error has occured initializing the database.")
@@ -49,4 +51,46 @@ class DBManager():
             exit(1)
         return False
     
-database = DBManager("./Storage/database.db")
+    def initialize_tables(self):
+        # Creates the required tables for the program to function.
+        # If the tables already exist, no changes will be made.
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("""CREATE TABLE accounts (
+            id            INTEGER    PRIMARY KEY AUTOINCREMENT,
+            username      TEXT (100) NOT NULL,
+            email         TEXT (512),
+            password_hash TEXT (256) NOT NULL,
+            date_created  INTEGER,
+            last_login    INTEGER,
+            account_type  TEXT (5) NOT NULL
+            );""")
+            self.connection.commit()
+        except Exception as e:
+            print(f"Alert: {e}. Continuting...")
+
+        try:
+            cursor.execute("""CREATE TABLE login_tokens (
+            id         INTEGER      PRIMARY KEY AUTOINCREMENT,
+            account_id              REFERENCES accounts (id) 
+                            NOT NULL,
+            access_token TEXT (256) NOT NULL,
+            date_created INTEGER,
+            expiry_date  INTEGER    NOT NULL
+            );""")
+            self.connection.commit()
+        except Exception as e:
+            print(f"Alert: {e}. Continuting...")
+
+        try:
+            cursor.execute("""CREATE TABLE reviews (
+            id         INTEGER     PRIMARY KEY AUTOINCREMENT,
+            account_id INTEGER     REFERENCES accounts (id) ON DELETE NO ACTION,
+            rating     INTEGER     NOT NULL,
+            comment    TEXT (8192),
+            location   TEXT (512) 
+            );""")
+            self.connection.commit()
+        except Exception as e:
+            print(f"Alert: {e}. Continuting...")
+    
