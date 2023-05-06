ac23d0f6b2d4cf86c4692d7f9541ecd9775e013a
CharTex <HarryAGearing@outlook.com>
2023-05-06 13:46:11 +0100

    Remove deprecated code

--- GameLog-App/App.js
+++ GameLog-App/App.js
@@ -10,30 +10,6 @@ import {
   ForgotPasswordScreen,
 } from "./screens"
 
-function Feed() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed!</Text>
-    </View>
-  );
-}
-
-function Profile() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
-    </View>
-  );
-}
-
-function Notifications() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
-    </View>
-  );
-}
-
 const Tab = createMaterialBottomTabNavigator();
 
 function LoginTabs() {
