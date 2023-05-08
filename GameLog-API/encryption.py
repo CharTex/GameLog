86662031e16a0d634372b3dd777c9730c7287cca
CharTex <HarryAGearing@outlook.com>
2023-05-08 13:52:56 +0100

    Change hash method to argon2.

--- GameLog-API/encryption.py
+++ GameLog-API/encryption.py
@@ -1,9 +1,11 @@
 from hashlib import sha256
+from passlib.hash import argon2
+import secrets
 
 def hash_password(password):
     # Returns the hash of the supplied password.
-    hasher = sha256()
-    password = password.encode('utf-8')
-    hasher.update(password)
-    hash = hasher.hexdigest()
-    return hash
+    hashed_password = argon2.hash(password)
+    return hashed_password
+def verify_password(password, hashed_password):
+    # Compares the provided password to the hashed version.
+    return argon2.verify(password, hashed_password)
